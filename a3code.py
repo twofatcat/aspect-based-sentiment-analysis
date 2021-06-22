e74ee31651a8a532ff2818de9780d71d9928e77e
twofatcat <35566450+twofatcat@users.noreply.github.com>
2021-06-22 13:49:03 +0800

    Delete a3code.py

--- a3code.py
+++
@@ -1,9 +0,0 @@
-from xml.dom import minidom
-
-xmldoc = minidom.parse('./Assignment3/Restaurants_Train.xml')
-itemlist = xmldoc.getElementsByTagName('item')
-print(len(itemlist))
-# print(itemlist[0].attributes['name'].value)
-for s in itemlist:
-    print(s.value)
-
